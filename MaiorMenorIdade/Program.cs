1cd3c38f08485d6f32d6a17d2de048324a2b4090
masfazan <msfazan@icloud.com>
2024-04-21 16:12:16 -0300

    Add project files.

---
+++ MaiorMenorIdade/Program.cs
@@ -0,0 +1,24 @@
+﻿int maiorIdade = 0, menorIdade = 0, idade=0;
+
+for (int i=1; i<=10; i++)
+{
+    Console.WriteLine("Digite a idade: ");
+    idade = int.Parse(Console.ReadLine());
+    {
+
+        if (idade >= 18)
+        {
+            maiorIdade++;
+        }
+
+        else
+        {
+            menorIdade++;
+        }
+    }
+  
+}
+{
+    Console.WriteLine("Quantidade de maiores de idade:" + maiorIdade);
+    Console.WriteLine("Quantidade de menores de idade" + menorIdade);
+}
